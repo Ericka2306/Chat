445afc4d94e6ef9884ee40efdf77f9477cddc328
Ericka2306 <114937300+Ericka2306@users.noreply.github.com>
2022-12-07 06:32:06 +0300

    Add files via upload

---
+++ Chat Socket/MainClient.java
@@ -0,0 +1,12 @@
+package affichage;
+
+import clients.*;
+public class MainClient {
+       public static void main(String args[]){  
+        // Client client = null;
+        // if (args.length != 2)
+        // System.out.println("Usage: java Client host port");
+        // else
+        Client c = new Client("localhost", 5000);
+   }
+}
---
+++ Chat Socket/MainServer.java
@@ -0,0 +1,15 @@
+package affichage;
+
+import server.*;
+public class MainServer {
+    public static void main(String args[]){
+
+        // Server server = null;
+        // if (args.length != 1){
+        //     System.out.println("Usage: java ChatServer port");
+        // }
+        // else{
+            Server s = new Server(5000);
+        // }
+    }
+}
